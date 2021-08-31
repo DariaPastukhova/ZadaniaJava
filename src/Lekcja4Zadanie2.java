220469534a381d98fade9be08db967033d75a1a6
Daria Pastukhova <dariapastuchova@gmail.com>
2021-08-31 16:35:19 +0200

    Lekcja 4

--- src/Lekcja4Zadanie2.java
+++ src/Lekcja4Zadanie2.java
@@ -1,20 +1,10 @@
 public class Lekcja4Zadanie2 {
     public static void main(String[] args) {
+        int[] tab = {1, 2, 3, 4, 5, 6};
+        // wynik: { 6, 5, 4, 3, 2, 1 }
 
-        int[] tab = new int[]{1, 2, 3, 4, 5, 6};
-        int[] arr = new int[tab.length];
-        // wynik: 6, 5, 4, 3, 2, 1
-
-        for (int i = 0; i < tab.length; i++)
+        for (int i = tab.length - 1; i >= 0; i--) {
             System.out.print(tab[i] + " ");
-        System.out.println();
-
-        for (int i = 0; i < tab.length; i++)
-            arr[i] = tab[tab.length - 1 - i];
-
-        for (int i = 0; i < arr.length; i++)
-            System.out.print(arr[i] + " ");
-
-        System.out.println();
+        }
     }
 }
