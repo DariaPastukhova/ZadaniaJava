139658d04db54eb4b1d27c0dbf2a73941b76d5b9
Daria Pastukhova <dariapastuchova@gmail.com>
2021-08-31 17:30:12 +0200

    Lekcja 4

--- src/Lekcja4Zadanie2.java
+++ src/Lekcja4Zadanie2.java
@@ -3,8 +3,12 @@ public class Lekcja4Zadanie2 {
         int[] tab = {1, 2, 3, 4, 5, 6};
         // wynik: { 6, 5, 4, 3, 2, 1 }
 
-        for (int i = tab.length - 1; i >= 0; i--) {
-            System.out.print(tab[i] + " ");
+        for (int i = 0; i < tab.length; i++){
         }
+            for (int i = tab.length - 1; i >= 0; i--) {
+                System.out.print(tab[i] + " ");
+        }
+            System.out.println();
     }
 }
+
